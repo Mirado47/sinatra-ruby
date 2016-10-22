844ed4757130c0b47f355c013dc23733509badd4
Logan Byers <lbyers47@gmail.com>
2016-10-22 17:56:11 -0500

    made a method and cleaned up the code

--- myapp.rb
+++ myapp.rb
@@ -1,8 +1,22 @@
 # myapp.rb
 require 'sinatra'
 require 'sinatra/reloader'
+Num = rand(100)
 
+guess = -1
+error = ""
 get '/' do
-    x = rand(100)
-    'This random number is ' + x.to_s
+    guess = params["guess"].to_i
+    error = checker
+    erb :index, :locals => {:num => Num, :error => error}
 end
+  def checker
+      if (Num < params['guess'].to_i)
+       "Too high"
+    elsif(Num > params['guess'].to_i)
+      "Too low"  
+    else
+      "Rock and Roll"
+    end
+      
+  end
